7af390dadcbdb36f2a4e94524a2cd3f2aa345c00
Chris Antenesse <chris@foundapp.com>
2014-02-14 11:06:02 -0800

    fixed some visual things to make pep8 compliant

--- bin/rbuilder.py
+++ bin/rbuilder.py
@@ -7,32 +7,43 @@ import shutil
 import popen2
 from optparse import OptionParser
 
+
 def get_options():
     optparser = OptionParser()
 
-    optparser.add_option("-r", "--requires", action="store", type="string", nargs=1)
-    optparser.add_option("-a", "--application", action="store", type="string", nargs=1)
-    optparser.add_option("-d", "--build_dir", action="store", type="string", nargs=1)
-    optparser.add_option("-v", "--version", action="store", type="string", nargs=1)
-    optparser.add_option("-R", "--release", action="store", type="string", nargs=1)
-    optparser.add_option("-i", "--install_dir", action="store", type="string", nargs=1)
-    optparser.add_option("-x", "--rpmbuild_dir", action="store", type="string", nargs=1)
-    optparser.add_option("-c", "--architecture", action="store", type="string", nargs=1)
-    
-    (options, unparsed) = optparser.parse_args()
-    if unparsed:
-        print("PROBLEM:"
-              "Could not parse and/or understand the following arguments:")
-        for arg in unparsed:
-            print(" - %s" % arg)
-        print()
-        parser.print_help()
-        sys.exit(-1)
+    optparser.add_option("-r", "--requires", action="store",
+                         type="string", nargs=1)
+    optparser.add_option("-a", "--application", action="store",
+                         type="string", nargs=1)
+    optparser.add_option("-d", "--build_dir", action="store",
+                         type="string", nargs=1)
+    optparser.add_option("-v", "--version", action="store",
+                         type="string", nargs=1)
+    optparser.add_option("-R", "--release", action="store",
+                         type="string", nargs=1)
+    optparser.add_option("-i", "--install_dir", action="store",
+                         type="string", nargs=1)
+    optparser.add_option("-x", "--rpmbuild_dir", action="store",
+                         type="string", nargs=1)
+    optparser.add_option("-c", "--architecture", action="store",
+                         type="string", nargs=1)
 
-    return (options.requires, options.application, 
-            options.build_dir, options.version, 
-            options.release, options.install_dir,
-            options.rpmbuild_dir, options.architecture)
+(options, unparsed) = optparser.parse_args()
+
+if unparsed:
+    print("PROBLEM:"
+          "Could not parse and/or understand the following arguments:")
+    for arg in unparsed:
+        print(" - %s" % arg)
+    print()
+    parser.print_help()
+    sys.exit(-1)
+
+return (options.requires, options.application,
+        options.build_dir, options.version,
+        options.release, options.install_dir,
+        options.rpmbuild_dir, options.architecture)
+
 
 def build_paths(rpmbuild_dir):
     rpmbuild_source = "%s/SOURCES" % rpmbuild_dir
@@ -41,9 +52,10 @@ def build_paths(rpmbuild_dir):
 
     return rpmbuild_source, rpmbuild_spec
 
-def create_spec_file(tar_filename, application, requires, 
-                        base_dir, version, release, 
-                        install_dir, arch):
+
+def create_spec_file(tar_filename, application, requires,
+                     base_dir, version, release,
+                     install_dir, arch):
     try:
         os.makedirs("%s/SPECS" % (base_dir))
     except OSError:
@@ -87,41 +99,45 @@ rsync -av . $RPM_BUILD_ROOT/%s/%s/
 
 %%post
 
-""" % (application, application, version, release, tar_filename, 
+""" % (application, application, version, release, tar_filename,
         requires_line, arch, install_dir, application,
-        install_dir, application, install_dir, 
+        install_dir, application, install_dir,
         application)
-    
+
     spec_filename = "%s.spec" % application
     os.chdir(base_dir)
     f = open("%s/%s" % (rpmbuild_spec, spec_filename), 'w+')
     f.write(spec_file)
     f.close()
-    
+
     return spec_filename
+
 
 def create_source_tar(rpmbuild_source, application, build_dir):
     try:
         os.makedirs("%s/%s/" % (rpmbuild_source, application))
     except OSError:
         pass
-        
-    tar = tarfile.open("%s/%s/%s.tar.gz" % (rpmbuild_source, application, application), "w:gz")
+
+    tar = tarfile.open("%s/%s/%s.tar.gz" % (rpmbuild_source,
+                                            application,
+                                            application), "w:gz")
     os.chdir("%s" % (build_dir))
     for root, dirs, files in os.walk("."):
-         if '.git' not in root:
-             for file in files:
-                 tar.add(join(root, file))
+        if '.git' not in root:
+            for file in files:
+                tar.add(join(root, file))
     tar.close()
     return "%s.tar.gz" % application
+
 
 def create_rpm(spec_filename, base_dir):
     package_name = spec_filename.split('.')
     r, w, e = popen2.popen3("rpmbuild --define '_topdir %s'\
                              --define '_sourcedir %s/SOURCES/%s' \
-                             -bb SPECS/%s.spec" % (base_dir, base_dir, 
-                                                    package_name[0],
-                                                    package_name[0]))
+                             -bb SPECS/%s.spec" % (base_dir, base_dir,
+                                                   package_name[0],
+                                                   package_name[0]))
     output = r.read()
     error = e.read()
 
@@ -129,18 +145,20 @@ def create_rpm(spec_filename, base_dir):
 
 if __name__ == "__main__":
     # get options and build paths
-    requires, application, git_repo, version, release, install_dir, rpmbuild_dir, arch = get_options()
+    (requires, application, git_repo,
+     version, release, install_dir, rpmbuild_dir, arch) = get_options()
+
     rpmbuild_source, rpmbuild_spec = build_paths(rpmbuild_dir)
 
     # create source tar, spec file and create rpm
     tar_filename = create_source_tar(rpmbuild_source, application, git_repo)
-    spec_filename = create_spec_file(tar_filename, application, requires, 
-                                        rpmbuild_dir, version, release, 
-                                        install_dir, arch)
+    spec_filename = create_spec_file(tar_filename, application, requires,
+                                     rpmbuild_dir, version, release,
+                                     install_dir, arch)
     package_name = create_rpm(spec_filename, rpmbuild_dir)
 
     # clean up the build env
     shutil.rmtree("%s/%s" % (rpmbuild_source, package_name))
 
-    print "Package Name: %s-%s-%s.rpm" % (package_name, version, 
-                                            release) 
+    print "Package Name: %s-%s-%s.rpm" % (package_name, version,
+                                          release)
